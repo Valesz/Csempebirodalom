f3f2fe9934c6570e5a9b78d9280008fe2c797f64
Csonka Valentin <valesz0823@gmail.com>
2024-04-20 12:15:49 +0200

    Added gallery to main
    You can now see beautiful pictures in main.

--- irenke-csempeboltja/src/app/pages/main/main.module.ts
+++ irenke-csempeboltja/src/app/pages/main/main.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
+import { GalleryModule } from 'ng-gallery';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { MainComponent } from './main.component';
   ],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    GalleryModule
   ]
 })
 export class MainModule { }

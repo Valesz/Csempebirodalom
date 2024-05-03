eafb9f45f0c9e73681e9222da51c51c87dc0cdc8
Valesz <46132248+Valesz@users.noreply.github.com>
2024-05-03 02:10:53 +0200

    Revert "Revert "Develop (#1)"" (#2)
    This reverts commit 8cca22a38fb4b5a8c887476bdf4ca998a6d3c8f2.

---
+++ irenke-csempeboltja/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'main',
+    loadChildren: () => import('./pages/main/main.module').then(m => m.MainModule)
+  },
+  {
+    path: 'shop',
+    loadChildren: () => import('./pages/shop/shop.module').then(m => m.ShopModule)
+  },
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full'
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
